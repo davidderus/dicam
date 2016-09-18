9ede68d59576c1fb8d8c9288dadbe7a66d7733d8
David Dérus <derus.david@gmx.fr>
2016-09-18 14:31:02 +0200

    [+] Adding quick build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+build() {
+  suffix=".go"
+
+  for f in "$@"
+  do
+    binary_name=${f%$suffix}
+    echo "= Building $binary_name"
+    go build -o "build/dicam-$binary_name" -ldflags "-s" $f
+  done
+
+  printf "= Built done!\n\n"
+  ls -lah build/*
+}
+
+build $1
