7a50b946a409fe00142e3429f1428822091683f0
David Dérus <derus.david@gmx.fr>
2016-09-18 14:15:11 +0200

    [+] Adding basic cli commands for cams

--- cli.go
+++ cli.go
@@ -1,6 +1,44 @@
-package dicam
+package main
 
 import (
   "os"
+  "fmt"
+  "strings"
   "github.com/urfave/cli"
 )
+
+func main()  {
+  cams := []string{"Cam 1", "Cam 2"}
+
+  app := cli.NewApp()
+  app.Name = "dicam-cli"
+  app.Usage = "Controls dicam processes and cams"
+
+  app.Commands = []cli.Command{
+    {
+      Name: "camera",
+      Aliases: []string{"c"},
+      Usage: "Interacts with a camera",
+      Subcommands: []cli.Command{
+        {
+          Name: "start",
+          Usage: "Starts a camera",
+          Action: func(c *cli.Context) error {
+            fmt.Println("Starting cam", c.Args().First())
+            return nil
+          },
+        },
+        {
+          Name: "list",
+          Usage: "Lists all available cameras",
+          Action: func(c *cli.Context) error {
+            fmt.Println(strings.Join(cams, "\n"))
+            return nil
+          },
+        },
+      },
+    },
+  }
+
+  app.Run(os.Args)
+}
