713328a708ced5bb5928ad98f073ea1b214a7fd0
David Dérus <derus.david@gmx.fr>
2016-09-25 11:46:46 +0200

    [*] Gofmt for cli and version external version number

--- cli.go
+++ cli.go
@@ -1,76 +1,77 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "strings"
-  "github.com/urfave/cli"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/urfave/cli"
 )
 
-func main()  {
-  cams := []string{"Cam 1", "Cam 2"}
+func main() {
+	cams := []string{"Cam 1", "Cam 2"}
 
-  app := cli.NewApp()
-  app.Name = "dicam-cli"
-  app.Usage = "Controls dicam processes and cams"
-  app.Version = "0.0.1"
+	app := cli.NewApp()
+	app.Name = "dicam-cli"
+	app.Usage = "Controls dicam processes and cams"
+	app.Version = dicamVersion
 
-  app.Commands = []cli.Command{
-    {
-      Name: "camera",
-      Aliases: []string{"c"},
-      Usage: "Interacts with a camera",
-      Subcommands: []cli.Command{
-        {
-          Name: "start",
-          Usage: "Starts a camera",
-          Action: func(c *cli.Context) error {
-            fmt.Println("Starting cam", c.Args().First())
-            return nil
-          },
-        },
-        {
-          Name: "stop",
-          Usage: "Stops a camera",
-          Action: func(c *cli.Context) error {
-            fmt.Println("Stopping cam", c.Args().First())
-            return nil
-          },
-        },
-        {
-          Name: "list",
-          Usage: "Lists all available cameras",
-          Action: func(c *cli.Context) error {
-            fmt.Println(strings.Join(cams, "\n"))
-            return nil
-          },
-        },
-      },
-    },
-    {
-      Name: "server",
-      Aliases: []string{"s"},
-      Usage: "Manages the webserver",
-      Subcommands: []cli.Command{
-        {
-          Name: "start",
-          Usage: "Starts the webserver",
-          Action: func(c *cli.Context) error {
-            fmt.Println("Starting webserver")
-            return nil
-          },
-        },
-        {
-          Name: "stop",
-          Usage: "Stops the webserver",
-          Action: func(c *cli.Context) error {
-            fmt.Println("Stopping webserver")
-            return nil
-          },
-        },
-      },
-    },
-  }
+	app.Commands = []cli.Command{
+		{
+			Name:    "camera",
+			Aliases: []string{"c"},
+			Usage:   "Interacts with a camera",
+			Subcommands: []cli.Command{
+				{
+					Name:  "start",
+					Usage: "Starts a camera",
+					Action: func(c *cli.Context) error {
+						fmt.Println("Starting cam", c.Args().First())
+						return nil
+					},
+				},
+				{
+					Name:  "stop",
+					Usage: "Stops a camera",
+					Action: func(c *cli.Context) error {
+						fmt.Println("Stopping cam", c.Args().First())
+						return nil
+					},
+				},
+				{
+					Name:  "list",
+					Usage: "Lists all available cameras",
+					Action: func(c *cli.Context) error {
+						fmt.Println(strings.Join(cams, "\n"))
+						return nil
+					},
+				},
+			},
+		},
+		{
+			Name:    "server",
+			Aliases: []string{"s"},
+			Usage:   "Manages the webserver",
+			Subcommands: []cli.Command{
+				{
+					Name:  "start",
+					Usage: "Starts the webserver",
+					Action: func(c *cli.Context) error {
+						fmt.Println("Starting webserver")
+						return nil
+					},
+				},
+				{
+					Name:  "stop",
+					Usage: "Stops the webserver",
+					Action: func(c *cli.Context) error {
+						fmt.Println("Stopping webserver")
+						return nil
+					},
+				},
+			},
+		},
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
