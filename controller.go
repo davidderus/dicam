394e71ca7e2c6bdde939acd1c67b69f571dacb10
David Dérus <derus.david@gmx.fr>
2016-09-25 12:48:23 +0200

    [*] Controlling a camera

--- controller.go
+++ controller.go
@@ -12,37 +12,51 @@ type controller struct {
 func (c *controller) launchCamera(cameraID int) camera {
 	cam := camera{id: cameraID}
 
-	c.cameras = append(c.cameras, cam)
-
 	cam.setup()
 
 	fmt.Printf("Starting cam %d\n", cam.id)
 	cam.start()
 
-	fmt.Printf("Camera started with PID %d\n", cam.command.Process.Pid)
+	fmt.Printf("Camera started with PID %d\n", cam.pid)
+
+	c.cameras = append(c.cameras, cam)
 
 	return cam
+}
+
+func (c controller) listCameras() []camera {
+	return c.cameras
+}
+
+func (c *controller) getCameraByID(cameraID int) *camera {
+	for _, cam := range c.cameras {
+		if cam.id == cameraID {
+			return &cam
+		}
+	}
+
+	panic("No camera found")
+}
+
+func (c *controller) stopCamera(cameraID int) {
+	fmt.Printf("Stopping cam %d\n", cameraID)
+
+	cam := c.getCameraByID(cameraID)
+	pid := cam.pid
+
+	cam.stop()
+
+	fmt.Printf("Camera stopped via PID %d\n", pid)
 }
 
 func main() {
 	mainController := controller{}
 
-	for i := 1; i < 6; i++ {
-		fmt.Println("Launching camera", i)
-		mainController.launchCamera(i)
-	}
+	mainController.launchCamera(1)
 
-	time.Sleep(10)
+	time.Sleep(5 * time.Second)
 
-	for i := 1; i < 4; i++ {
-		fmt.Println("Stopping camera", i)
-		mainController.cameras[i].stop()
-	}
+	fmt.Println(mainController.listCameras())
 
-	time.Sleep(10)
-
-	for i := 4; i < 6; i++ {
-		fmt.Println("Stopping camera", i)
-		mainController.cameras[i].stop()
-	}
+	mainController.stopCamera(1)
 }
