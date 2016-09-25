5f7e395572f8d997267415d94269c804c1825af5
David Dérus <derus.david@gmx.fr>
2016-09-25 22:26:31 +0200

    [+] Adding basic command server

---
+++ command-server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type commandServer struct {
+	host string
+	port int
+}
+
+func (cs *commandServer) start() error {
+	service := fmt.Sprintf("%s:%d", cs.host, cs.port)
+	tcpAddress, resolveError := net.ResolveTCPAddr("tcp4", service)
+	if resolveError != nil {
+		return resolveError
+	}
+
+	listener, listenError := net.Listen("tcp", tcpAddress.String())
+	if listenError != nil {
+		return listenError
+	}
+
+	defer listener.Close()
+
+	for {
+		connection, acceptError := listener.Accept()
+		if acceptError != nil {
+			return acceptError
+		}
+
+		go handleConnection(connection)
+	}
+}
+
+func handleConnection(connection net.Conn) {
+	buffer := make([]byte, 1024)
+	connection.Read(buffer)
+
+	connection.Write([]byte("hello from dicam\n"))
+}
