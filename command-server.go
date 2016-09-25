20b3be87f12c58776d73176e2b91a5651a09d280
David Dérus <derus.david@gmx.fr>
2016-09-25 23:19:22 +0200

    [+] Adding logic to handle cam and server jobs

--- command-server.go
+++ command-server.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
+	"strings"
 )
+
+// Server logic
 
 type commandServer struct {
 	host string
 	port int
+}
+
+func requestError(connection net.Conn, errorMessage string) {
+	message := "ERROR"
+
+	if errorMessage != "" {
+		message = fmt.Sprintf("%s-%s", message, errorMessage)
+	}
+
+	connection.Write([]byte(message + "\n"))
+	connection.Close()
 }
 
 func (cs *commandServer) start() error {
@@ -30,14 +46,69 @@ func (cs *commandServer) start() error {
 			return acceptError
 		}
 
-		go handleConnection(connection)
+		go handleCommand(connection)
 	}
 }
 
-func handleConnection(connection net.Conn) {
-	buffer := make([]byte, 1024)
-	connection.Read(buffer)
+func handleCommand(connection net.Conn) {
+	message, bufferError := bufio.NewReader(connection).ReadString('\n')
 
-	connection.Write([]byte("hello from dicam\n"))
+	if bufferError != nil {
+		requestError(connection, "")
+	}
+
+	parsedCommand := parseCommand(strings.TrimRight(string(message), "\n"))
+
+	// todo Return and handle any error
+	commandRunner(parsedCommand)
+
 	connection.Close()
+}
+
+func parseCommand(command string) CommandInterface {
+	commandArray := strings.Split(command, "-")
+
+	if len(commandArray) > 1 {
+		command := commandArray[0]
+		args := commandArray[1:]
+
+		switch command {
+		case "CAM":
+			return CamCommand{Command{args}}
+		case "SERVER":
+			return ServerCommand{Command{args}}
+		default:
+			return InvalidCommand{Command{}}
+		}
+	}
+}
+
+func commandRunner(command CommandInterface) {
+	command.run()
+}
+
+// Command Handling
+
+type CommandInterface interface {
+	run() string
+}
+
+type Command struct{ params []string }
+
+type CamCommand struct{ Command }
+
+type ServerCommand struct{ Command }
+
+type invalidCommand struct{ Command }
+
+func (com CamCommand) run() (string, error) {
+	return "", nil
+}
+
+func (com ServerCommand) run() (string, error) {
+	return "", nil
+}
+
+func (com invalidCommand) run() (string, error) {
+	return "", errors.New("Invalid command")
 }
