06cbff0859b493a26d204b307982d5199b8060b4
David Dérus <derus.david@gmx.fr>
2016-12-18 20:02:55 +0100

    [*] Asking client for cams list

--- server/camera-index.go
+++ server/camera-index.go
@@ -3,10 +3,13 @@ package server
 import (
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 // CameraIndex lists all cameras
 func CameraIndex(w http.ResponseWriter, r *http.Request) {
-	// askClient("CAM-LIST")
-	writeWithTemplate(w, "CameraIndex", filepath.Join("cameras", "index.html"), nil)
+	camsRawList := askClient("CAM-LIST")
+	camsList := strings.Split(camsRawList, "\n")
+
+	writeWithTemplate(w, "CameraIndex", filepath.Join("cameras", "index.html"), camsList)
 }
